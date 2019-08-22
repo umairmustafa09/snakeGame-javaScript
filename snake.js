4d4d40fe34f11e4e8628c5aee5bedcc1a5ad0a2e
Umair Mustafa <umairhassaaan@gmail.com>
2019-08-22 23:41:17 +0500

    latest

--- snake.js
+++ snake.js
@@ -1,68 +1,134 @@
 const convas = document.getElementById( "canvas" );
 const context = canvas.getContext( '2d' );
 
-let snake = [ { x: 20, y: 20 } ];  // snake array of object.
+// snake array of object.
+let snake = [ { x: 20, y: 20 } ];  
+// apple array of object.
 let apple = { x: 20, y: 20 };
-let axis = [ { x: 0, y: 0 } ];  //axis array of object for snake.
+ //axis array of object for snake.
+let axis = [ { x: 0, y: 0 } ]; 
+ //axis array of object for Apple.
 let appleAxis = { x: 0, y: 0 };
-let code = 0;
+//variable intilization.
+let code = 0, prevCode = 0 , newCode = 0, intId= 0, count = 0;
 
+//snake head on rest state.
 context.fillRect( axis.x, axis.y, snake.x, snake.y ); //on rest state showing snkae head.
+//start interval on the time of pageLoad.
+intId = setInterval( continuoulyMove, 150 );
+//calling apple funciton.
+RandomApple();
 
+//move snake function.
 function movesnake(){
-    context.clearRect(0 ,0 , 500, 500); //canvas height and width
+    //clear rect 500 is canvas height and width 
+    context.clearRect(0 ,0 , 500, 500); 
+    //background for canvas.
+    context.fillStyle = "white";
+    context.fillRect( 0, 0, canvas.width, canvas.height ); 
+    //drawing apple on canvas.
     context.fillStyle = "black";
-    context.fillRect( appleAxis.x, appleAxis.y, apple.x, apple.y ); //drawing snake at new position.
-    context.fillStyle = "red"; // color for snake.
-    for( let i = 0; i < snake.length; i++ ){
+    context.fillRect( appleAxis.x, appleAxis.y, apple.x, apple.y );
+    //drawing snake head on canvas.
+    context.fillStyle = "blue";
+    context.fillRect(  axis[ 0 ].x, axis[ 0  ].y, snake[ 0 ].x, snake[ 0 ].y ); 
+    //drawing snake body on canvas.
+    context.fillStyle = "red"; 
+    for( let i = 1; i < snake.length; i++ ){
         context.fillRect( axis[ i ].x, axis[ i  ].y, snake[ i ].x, snake[ i ].y ); //drawing snake at new position.
     }
 }
 
+//random Apple function.
 function RandomApple(){
-    appleAxis.x = Math.floor((Math.random() * 480) / 20);
-    appleAxis.y = Math.round(( Math.random() * 480) / 20 );
+    //caluculating Apple x and y axis.
+    appleAxis.x = Math.floor(( Math.random() * 480 ) / 20);
+    appleAxis.y = Math.round(( Math.random() * 480 ) / 20 );
     appleAxis.x = appleAxis.x * 20;
     appleAxis.y = appleAxis.y * 20;
 }
 
+//when key press acting according to it.
 window.onkeydown = function ( event ) {
-    code = event.keyCode;
+    //condition to act on only when W, A, S and D key pressed
+    if( event.keyCode == 65 || event.keyCode == 68 || event.keyCode == 87 || event.keyCode == 83 )
+    //add key code to new Code Variable.
+    newCode = event.keyCode;
+    //condtion to not move in opposite direction.
+    if( newCode == 65 && prevCode == 68 ) prevCode = 65;
+    else if( newCode == 68 && prevCode == 65 ) prevCode = 68;
+    else if( newCode == 83 && prevCode == 87 )  prevCode = 83;
+    else if(newCode == 87 && prevCode == 83) newCode = 83;
+
+    if( prevCode != newCode )
+        code = event.keyCode;
 }
 
 function continuoulyMove(){
+    //for loop for snake lenght to 0.
     for( let i = snake.length - 1; i >= 0 ; i-- ){
         if( i == 0 ){
-            if( code == 83){
-                if( axis[ i ].y < 480 )   //canvas height - snake intial height.
+            if( code == 83 ){
+                //canvas height - snake intial height.
+                if( axis[ i ].y < 480 )  
                     axis[ i ].y += snake[ i ].y;
             }
-            else if( code == 68){
-                if( axis[ i ].x < 480)     //canvas width - snake intial width.
+            else if( code == 68 ){
+                 //canvas width - snake intial width.
+                if( axis[ i ].x < 480)    
                     axis[ i ].x += snake[ i ].x;
             }
-            else if( code == 65){
-                if( axis[ i ].x > 0)        //canvas intitial width. 
+            else if( code == 65 ){
+                //canvas intitial width. 
+                if( axis[ i ].x > 0)        
                     axis[ i ].x -= snake[ i ].x;
             }
-            else if( code == 87){
-                if( axis[ i ].y >  0)       //canvas intitial height.
+            else if( code == 87 ){
+                 //canvas intitial height.
+                if( axis[ i ].y >  0)      
                     axis[ i ].y -= snake[ i ].y;
             }
-            if( axis[ 0 ].x == appleAxis.x && axis[ 0 ].y == appleAxis.y){
-                snake.push( {} );
-                axis.push( {} );
-                RandomApple();
+            //condtion for if apple is collide with snake head.
+            if( axis[ 0 ].x == appleAxis.x && axis[ 0 ].y == appleAxis.y ) 
+                EatApple();
+            //condition for if snake collide with its body.
+            for( let j = 1;  j < snake.length; j++ ){
+                if( axis[ 0 ].x == axis[ j ].x && axis[ 0 ].y == axis[ j ].y ){
+                    gameOver();
+                }
             }
         }
         else{
+            //movind snake body to follow snake head.
             snake[ i ].x = snake[ i - 1 ].x;
             snake[ i ].y = snake[ i - 1 ].y;
             axis[ i ].x = axis[ i - 1 ].x;
             axis[ i ].y = axis[ i - 1 ].y;
         }
+        //adding value to snake code.
+        prevCode = code;
     }
+    //calling move snake function.
     movesnake();
 }
-let id = setInterval( continuoulyMove, 150 );
-RandomApple();
+
+// eat snake function.
+function EatApple(){
+    //pushing values in snake array.
+    snake.push( {} );
+    //pushing values in snake axis array.
+    axis.push( {} );
+    //calling random apple function to generate apple in random posiition.
+    RandomApple();
+    //counting the score of user.
+    count++;
+    //showing score on borwser.
+    document.getElementById( "score" ).innerHTML = "<h2>Score: " + count + " </h2>";
+    localStorage.setItem("count:", count);
+}
+//game over function.
+function gameOver(){
+    //clearing interval.
+    clearInterval( intId );
+    document.getElementById( "gameOver" ).innerHTML = "<h2>Game Over</h2>";
+}
